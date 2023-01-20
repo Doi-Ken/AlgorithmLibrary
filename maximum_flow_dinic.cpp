429d88e675eaceff3f22d5d398c0f5766d8bf57f
k2-doi <k2-doi@nri.co.jp>
2023-01-20 21:06:21 +0900

    maximumflow_dinic fixed

--- maximum_flow_dinic.cpp
+++ maximum_flow_dinic.cpp
@@ -35,7 +35,7 @@ double PI = 3.14159265358979323846;
 // 始点0, 終点V-1
 
 int V, E;
-struct edge { int to, cap, rev; };
+struct edge { int to;  long long cap; int rev; };
 
 vector<edge> G[MAX_N];
 int level[MAX_N];
@@ -50,7 +50,7 @@ void init(int n) {
 }
 
 // from からtoへ向かう容量capの辺をグラフに追加する
-void add_edge(int from, int to, int cap) {
+void add_edge(int from, int to, long long cap) {
 	edge e1 = { to, cap, G[to].size() };
 	G[from].push_back(e1);
 	edge e2 = { from, 0, G[from].size() - 1 };
@@ -78,15 +78,15 @@ void bfs(int s) {
 
 
 // 増加パスをDFSで探す
-int dfs(int v, int t, int f) {
+long long dfs(int v, int t, long long f) {
 	if (v == t) {
 		return f;
 	}
 
-	for (int &i = iter[v]; i < G[v].size(); i++) {
+	for (int& i = iter[v]; i < G[v].size(); i++) {
 		edge& e = G[v][i];
 		if (e.cap > 0 && level[v] < level[e.to]) {
-			int d = dfs(e.to, t, min(f, e.cap));
+			long long d = dfs(e.to, t, min(f, e.cap));
 			if (d > 0) {
 				e.cap -= d;
 				G[e.to][e.rev].cap += d;
@@ -99,16 +99,16 @@ int dfs(int v, int t, int f) {
 
 
 // sからtへの最大流を求める
-int max_flow(int s, int t) {
-	int flow = 0;
+long long max_flow(int s, int t) {
+	long long flow = 0;
 	while (true) {
 		bfs(s);
 		if (level[t] < 0) {
 			return flow;
 		}
 		memset(iter, 0, sizeof(iter));
-		int f;
-		while ((f = dfs(s, t, INF)) > 0) {
+		long long f;
+		while ((f = dfs(s, t, LINF)) > 0) {
 			flow += f;
 		}
 	}
